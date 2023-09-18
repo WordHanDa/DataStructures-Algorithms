2f980c77cc6686e61ae174e87ec4b59c110b603b
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-09-18 22:42:04 +0800

    Update main.cpp

--- HW01_B1129012_02/HW01_B1129012_02/main.cpp
+++ HW01_B1129012_02/HW01_B1129012_02/main.cpp
@@ -6,41 +6,31 @@ int main() {
     int x;
     cout << "輸入X筆名字：";
     cin >> x;
-
-    int row = x;
-    int col = 2;
-    size_t maxNameLength = 0;
-
-    string** A = new string*[row];
-    for (int i = 0; i < row; i++) {
-        A[i] = new string[col];
+    int row=x;
+    int col=2;
+    size_t maxNameLength=0;
+    string** A=new string*[row];
+    for (int i=0; i<row; i++){
+        A[i]=new string[col];
     }
-
-    for (int i = 0; i < row; i++) {
+    for (int i=0; i<row; i++){
         cout << "輸入名字:";
-        for (int j = 0; j < col; j++) {
+        for (int j=0; j<col; j++){
             cin >> A[i][j];
         }
-        long int nameLength = A[i][0].length();
-        if (nameLength > maxNameLength) {
-            maxNameLength = nameLength;
-        }
+        long int nameLength=A[i][0].length();
+        maxNameLength=(nameLength>maxNameLength)?nameLength:maxNameLength;
     }
-
-    for (int i = 0; i < row; i++) {
-        
-        long int spacesToAdd = maxNameLength-A[i][0].length();
-        for (int j = 0; j < spacesToAdd; j++) {
+    for (int i=0; i<row; i++){
+        long int spacesToAdd=maxNameLength-A[i][0].length();
+        for (int j=0; j<spacesToAdd; j++){
             cout << " ";
         }
-        cout << A[i][0];
-        cout << " " << A[i][1] << endl;
+        cout << A[i][0] << " " << A[i][1] << endl;
     }
-
-    for (int i = 0; i < row; i++) {
+    for (int i=0; i<row; i++){
         delete[] A[i];
     }
     delete[] A;
-
     return 0;
 }
