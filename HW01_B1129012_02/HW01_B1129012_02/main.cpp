bf88a5bbb769a0dcead57949ef6e4545cfb02ea6
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-09-18 21:35:01 +0800

    Create main.cpp

---
+++ HW01_B1129012_02/HW01_B1129012_02/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW01_B1129012_02
+//
+//  Created by macbook pro on 2023/9/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
