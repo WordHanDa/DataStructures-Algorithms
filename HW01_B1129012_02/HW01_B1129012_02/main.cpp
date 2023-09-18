70b3d71e9618c5b41e4b0d9224486ce100d0d40a
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-09-18 21:37:37 +0800

    Update main.cpp

--- HW01_B1129012_02/HW01_B1129012_02/main.cpp
+++ HW01_B1129012_02/HW01_B1129012_02/main.cpp
@@ -1,14 +1,60 @@
-//
-//  main.cpp
-//  HW01_B1129012_02
-//
-//  Created by macbook pro on 2023/9/18.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+void swap(string &a, string &b){
+    string temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubbleSort(string** arr, int n) {
+    for (int i=0; i<n-1; i++) {
+        for (int j=0; j<n-i-1; j++) {
+            if (int(arr[j][0][0])<int(arr[j+1][0][0])) {
+                swap(*(arr+j), *(arr+j+1));
+            }
+        }
+    }
+}
 
 int main() {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int x, y;
+    cout << "輸入X個字串，字串不可超過Y個字(X,Y):";
+    cin >> x >> y;
+    int row = x;
+    int col = 1;
+
+    string** A = new string* [row];
+    for(int i=0; i<row; i++){
+        A[i]=new string[col];
+    }
+
+    for(int i=0; i<row; i++) {
+        for (int j=0; j<col; j++) {
+            cout << "輸入字串：";
+            cin >> A[i][j];
+            while (A[i][j].length()>y) {
+                cin.clear();
+                cout << "字串長度超過" << y << "個字，重新輸入" << endl;
+                cin >> A[i][j];
+            }
+        }
+    }
+
+    bubbleSort(A, row);
+
+    for (int i=0; i<row; i++) {
+        for (int j=0; j<col; j++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i=0; i<row; i++) {
+        delete[] A[i];
+    }
+    delete[] A;
+
     return 0;
 }
